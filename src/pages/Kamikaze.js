1753e56ebd2938bd764299facc4e93d00adb6ebb
sergio <sergiolopezantequera@gmail.com>
2025-02-16 08:57:01 +0100

    Create Kamikaze.js

---
+++ src/pages/Kamikaze.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Kamikaze() {
+  return (
+    <div className="App-main p-6 text-white">
+      <h2 className="text-3xl mb-6">Kamikaze</h2>
+      <p>Información sobre el Kamikaze...</p>
+    </div>
+  );
+}
+
+export default Kamikaze;
