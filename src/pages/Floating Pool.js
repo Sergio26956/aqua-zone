c3b26f7318c80c09077464c80d473a058fcf9af8
sergio <sergiolopezantequera@gmail.com>
2025-02-16 08:49:59 +0100

    Create Floating Pool.js

---
+++ src/pages/Floating Pool.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FloatingPool() {
+  return (
+    <div className="App-main p-6 text-white">
+      <h2 className="text-3xl mb-6">Piscina Flotante</h2>
+      <p>Información sobre la piscina flotante...</p>
+    </div>
+  );
+}
+
+export default FloatingPool;
