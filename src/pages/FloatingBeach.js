28ef5fe3a8869100b1ab7fc4ea9f0aaeb789709f
sergio <sergiolopezantequera@gmail.com>
2025-02-16 08:52:23 +0100

    Create FloatingBeach.js

---
+++ src/pages/FloatingBeach.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FloatingBeach() {
+  return (
+    <div className="App-main p-6 text-white">
+      <h2 className="text-3xl mb-6">Playa Flotante</h2>
+      <p>Información sobre la playa flotante...</p>
+    </div>
+  );
+}
+
+export default FloatingBeach;
