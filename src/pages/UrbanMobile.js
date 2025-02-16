907c25562fd66beec7e69cc267ae35db4557abef
sergio <sergiolopezantequera@gmail.com>
2025-02-16 08:59:06 +0100

    Create UrbanMobile.js

---
+++ src/pages/UrbanMobile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UrbanMobile() {
+  return (
+    <div className="App-main p-6 text-white">
+      <h2 className="text-3xl mb-6">Parque Acuático Urbano Móvil</h2>
+      <p>Información sobre el parque acuático urbano móvil...</p>
+    </div>
+  );
+}
+
+export default UrbanMobile;
