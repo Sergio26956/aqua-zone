43bf6b3bd4c195018dfa18a34e75cebc597f1b25
sergio <sergiolopezantequera@gmail.com>
2025-02-16 08:43:06 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-blue-700 p-4 text-white mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 AQUAZONE. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
