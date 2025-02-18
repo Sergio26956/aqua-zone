25cd5d12875b5694bcf5f26ba69f710a355049e1
sergio <sergiolopezantequera@gmail.com>
2025-02-18 15:14:25 +0100

    última actualizacion index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
-import './index.css'; // Importar Tailwind CSS
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import FloatingPool from './pages/FloatingPool';
-import FloatingBeach from './pages/FloatingBeach';
-import Kamikaze from './pages/Kamikaze';
-import UrbanMobile from './pages/UrbanMobile';
-import WaterPark from './pages/WaterPark';
-import LandWaterPark from './pages/LandWaterPark';
-import Contact from './pages/Contact';
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/floating-pool" component={FloatingPool} />
-      <Route path="/floating-beach" component={FloatingBeach} />
-      <Route path="/kamikaze" component={Kamikaze} />
-      <Route path="/urban-mobile" component={UrbanMobile} />
-      <Route path="/water-park" component={WaterPark} />
-      <Route path="/land-water-park" component={LandWaterPark} />
-      <Route path="/contact" component={Contact} />
-    </Switch>
-  </Router>,
+  <React.StrictMode>
+    <App />
+    <FloatingPool />
+  </React.StrictMode>,
   document.getElementById('root')
 );
