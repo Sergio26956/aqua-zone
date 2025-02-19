987a246e002afb3c8828bdfa4614b0767bc18452
sergio <sergiolopezantequera@gmail.com>
2025-02-19 10:15:42 +0100

    actualizar index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import FloatingPool from './pages/FloatingPool';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <FloatingPool />
   </React.StrictMode>,
   document.getElementById('root')
 );
