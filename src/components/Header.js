bf2b255ef835cdee565b015d2c25b65bec56ef4b
sergio <sergiolopezantequera@gmail.com>
2025-02-19 09:46:22 +0100

    actualizar Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './Header.css';
 
-function Header() {
+const Header = () => {
   return (
-    <header className="bg-blue-700 p-4 text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl">AQUAZONE</h1>
-        <nav>
-          <Link to="/" className="mr-4">Inicio</Link>
-          <Link to="/water-park" className="mr-4">Parque Acuático</Link>
-          <Link to="/land-water-park" className="mr-4">Parque Acuático Terrestre</Link>
-          <Link to="/contact" className="mr-4">Contacto</Link>
-        </nav>
-      </div>
+    <header className="header">
+      <h1>Aqua Zone</h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
