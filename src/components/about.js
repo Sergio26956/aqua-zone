f8b08fa5c8d19b836a795841802f33a6b7ae1df1
sergio <sergiolopezantequera@gmail.com>
2025-02-19 09:58:37 +0100

    Create about.js

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <h2>About Us</h2>
+      <p>Aqua Zone is dedicated to providing information and resources about aquatic life.</p>
+    </section>
+  );
+};
+
+export default About;
