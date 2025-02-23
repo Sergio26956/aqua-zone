246968c95707b7a76d9b95e6dca2d1e1da1ddc44
sergio <sergiolopezantequera@gmail.com>
2025-02-23 09:28:40 +0100

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <section id="home" className="home">
+      <h2>Welcome to Aqua Zone</h2>
+      <p>Your go-to place for all things related to aquatic life.</p>
+    </section>
+  );
+};
+
+export default Home;
