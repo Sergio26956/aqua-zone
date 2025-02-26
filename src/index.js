be99eb0ca772626e4f675b485ae9651f183f003c
sergio <sergiolopezantequera@gmail.com>
2025-02-26 17:04:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Cambié esto
 import './styles/index.css';
 import App from './App';
 
-ReactDOM.render(
+// Crear un root para la aplicación
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderizar el componente <App />
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
