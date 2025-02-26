ef146d5fccfc487d5f476d92468e2a5cce9d4cc2
sergio <sergiolopezantequera@gmail.com>
2025-02-26 17:13:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';  // Si index.css está en la raíz de 'src'
-import App from './App';  // Si App.js está en la raíz de 'src'
+import './styles/index.css';  // Ruta correcta si 'index.css' está en la subcarpeta 'styles'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />  {/* Este es tu componente principal que se renderiza en el div con id 'root' */}
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')  // Este es el div en el archivo index.html donde se renderiza la app
+  document.getElementById('root')  // Aquí se renderiza tu aplicación en el div con id 'root'
 );
