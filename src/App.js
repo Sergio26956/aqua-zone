3de92c0e36482ecb9314578b79721670d902fc5a
sergio <sergiolopezantequera@gmail.com>
2025-02-26 17:15:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './styles/App.css';
+import './styles/App.css';  // Ruta correcta si 'App.css' está en la subcarpeta 'styles'
 
-const App = () => {
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <Home />
-        <About />
-        <Contact />
-      </main>
-      <Footer />
+    <div className="App">
+      <h1>Bienvenido a AQUAZONE</h1>
+      {/* Tu contenido de la app aquí */}
     </div>
   );
-};
+}
 
 export default App;
